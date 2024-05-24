6a83455b1a2d13ef48c13f99836a1b22c1ea52d6
Matias-Alessandrello <matialess@gmail.com>
2024-05-24 16:28:01 -0300

    con el metodo post

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 import requests
 
-URL = 'https://httpbin.dev/get'
+URL = 'https://httpbin.dev/post'
 
-params = {
+
+data = { # lo use con el post
+    'username': 'matias',
+    'password': 'password123'
+    
+}
+"""
+params = { # lo use con el get
     'name': 'Matias Aless',
     'password': '123',
     'email': 'mati@codigofacilito.com'
 }
-
-response = requests.get(URL, params=params) # se manda la query atravez del diccionario params, para información sensible, como la usamos POST 
+"""
+response = requests.post(URL, data=data) # se manda la query atravez del diccionario params, para información sensible, como la usamos POST 
 
 if response.status_code == 200:
     payload = response.json()
-    params = payload['args']
+# va con el get   params = payload['args']
     
+    print(response.text)
+    print(response.url)
+    
+"""    
     print(params['name'])
     print(params['password'])
     print(params['email'])
+""" 
     
-    
-    print(response.url)
